82b1f6aa47ba7a7c2fcd959d56bb0736101eb121
Ronnie Nigash <nigash@uw.edu>
2015-05-03 18:23:36 -0700

    Change Calculator to have Bubble sort Capabilities

--- simpleCalc.cpp
+++ simpleCalc.cpp
@@ -2,6 +2,20 @@
 #include <stdio.h>      /* printf */
 #include <math.h>
 
+void bubbleSort(int *ptr, int k)
+{
+	int temp;
+	for (int i = 1; i < k; i++) {
+		for (int j = 0; j < (k - 1); j++) {
+			if (*(ptr + j) > *(ptr + j + 1)){
+				temp = *(ptr + j);
+				*(ptr + j) = *(ptr + j + 1);
+				*(ptr + j + 1) = temp;
+			}
+		}
+	}
+}
+
 bool quadratic(double a, double b, double c, double& positive, double& negative)
 {
 	double det = b*b-4.0*a*c;
@@ -23,20 +37,44 @@ void print(double a)
 	std::cout << "Result = " << a << std::endl;
 }
 
+
+
+
+
 int main()
 {
 	double a, b, c;
 
 	double pos;
 	double neg;
-	std::cout << "Quadratic Formula. Please enter a, b, c coefficients: ";
+	printf("Let's do some Quadratic Formula! Enter coefficients a, b ,c: ");
 	std::cin >> a >> b >> c;
-	printf("Our function is: %fx^2+%fx+%f\n", a, b, c);
+	printf("Our function is: %.1fx^2+%.1fx+%.1f\n", a, b, c);
 	if(quadratic(a,b,c,pos,neg)){
-		printf("Our positive root is: %f\n", pos);
-		printf("Our negative root is: %f", neg);
+		printf("Our positive root is: %.1f\n", pos);
+		printf("Our negative root is: %.1f\n", neg);
 	} else {
-		printf("No solutions found.");
+		printf("No solutions found.\n");
+	}
+	printf("\nLet's sort some arrays with Bubble Sort!\n");
+	int list[10];
+	int size;
+	printf("Enter the number of values: ");
+	scanf("%d", &size);
+	printf("\nEnter the element(s): ");	
+	for (int i = 0; i < size; i++) {
+		scanf("%d", &list[i]);
+	}
+
+	printf("\nBefore Bubble Sort!:\n");
+	for (int i = 0; i < size; i++) {
+		printf("%d   ", list[i]);
+	}
+	bubbleSort(list, size);
+
+	printf("\nAfter!:\n");
+	for (int i = 0; i < size; i++) {
+		printf("%d   ", list[i]);
 	}
 	return 0;
 }
