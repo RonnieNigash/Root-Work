47fd62c54506d971d5a3914f88fc81343c7bb835
Ronnie Nigash <nigash@uw.edu>
2015-06-16 12:39:21 -0700

    Add histogram_in_tree_branches.C for generating tree with branches of distributions

---
+++ treeWork/histogram_in_tree_branches.C
@@ -0,0 +1,8 @@
+void histogram_in_tree_branches() {
+	// Creates a Tree with two branches -- each branch generates 10,000 leaves
+	// Values at leaves are Gaussian distribution and Poisson
+	
+	// We will use PoissonD and Rannor function calls from API
+	// https://root.cern.ch/root/html600/TRandom.html#TRandom:Rannor
+
+}
