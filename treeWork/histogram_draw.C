e90397921e31d2767cb066a696beb2e8d436fe23
Ronnie Nigash <nigash@uw.edu>
2015-06-18 14:00:41 -0700

    Debug. Never getting inside of treeReader loop to read file

--- treeWork/histogram_draw.C
+++ treeWork/histogram_draw.C
@@ -1,23 +1,31 @@
+#include "TFile.h"
+#include "TH1F.h"
+#include "TTreeReader.h"
+#include "TTreeReaderValue.h"
+
 void histogram_draw() {
 	// Create histogram
 	TH1F *histo = new TH1F("histo", "myTree", 10000, -2,2);
 
 	// Open file with tree
-	TFile *file = TFile::Open("histo.root");
-	
-	// Get us out of here if our file is invalid!
-	if (!file || file->IsZombie()) {
-		cout << "File not found.";
-		return;
-	}
+//	TFile *file = TFile::Open("/users/ronnie/root/bin/histo.root");
+	TFile file("/users/ronnie/root/bin/histo.root");
 
-	TTreeReader treeReader("myTree", file);
-	
+	// Get us out of here if our file is invalid!
+//	if (!file || file->IsZombie()) {
+//		cout << "File not found.";
+//		return;
+//	}
+
+	TTreeReader treeReader("myTree", &file);
+		
 	// Names of branches
 	TTreeReaderValue<Float_t> valX(treeReader, "X");
 //	TTreeReaderValue<Float_t> valY(treeReader, "Y");
-	// @TODO: Fix this, doesn't appear to be filling out a tree -- cannot get values
+	// @TODO: We are never getting inside of this loop. Fix that!
+	cout << "We made it on the outside of the loop";
 	while(treeReader.Next()) {
+		cout << "We made it to the inside of the loop";
 		histo->Fill(*valX);
 	}
 
