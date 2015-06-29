c85ad993b89a383b1202894c7adbd5eec2c239b8
Ronnie Nigash <nigash@uw.edu>
2015-06-29 13:19:04 -0700

    Update for two branches of tree

--- treeWork/histogram_draw.C
+++ treeWork/histogram_draw.C
@@ -5,37 +5,26 @@
 
 void histogram_draw() {
 	// Create histogram
-	TH1F *histo = new TH1F("histo", "tree", 10000, -2,2);
+	TH1F *histoX = new TH1F("histoY", "tree", 10000, -2,2);
+	TH1F *histoY = new TH1F("histoY", "tree", 10000, 0, 4);
 
 	// Open file with tree
 	TFile *file = new TFile("histo.root","READ");
-//	TFile *file = TFile::Open("/users/ronnie/root/bin/histo.root");
-//	TFile file("/users/ronnie/root/bin/histo.root");
-//	Tfile file("hist.root");
-	// Taken from local $ROOTSYS/bin directory or nah?
 
 	// Get us out of here if our file is invalid!
 	if (file->IsOpen()) {
 		cout << "File opened!";
+	} else {
+		return;
 	}
-	
-
-	gFile = file;
-//	if (!file || file->IsZombie()) {
-//		cout << "File not found.";
-//		return;
-//	}
 
 	TTreeReader treeReader("tree", file);
-//	TTreeReader treeReader("My Tree", &file);
-		
+
 	// Names of branches
 	TTreeReaderValue<Float_t> valX(treeReader, "X");
-//	TTreeReaderValue<Float_t> valY(treeReader, "Y");
-	// @TODO: We are never getting inside of this loop. Fix that!
-	cout << "We made it on the outside of the loop";
+	TTreeReaderValue<Float_t> valY(treeReader, "Y");
+
 	while(treeReader.Next()) {
-		cout << "We made it to the inside of the loop";
 		histo->Fill(*valX);
 	}
 
